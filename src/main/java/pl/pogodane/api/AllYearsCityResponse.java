54de2592bf464b0fea3b67475d52c5cf7f51b47f
Miszczynski, Krzysztof <Krzysztof.Miszczynski@sabre.com>
2020-02-06 21:53:55 +0100

    All yearly data - added summaries.

--- src/main/java/pl/pogodane/api/AllYearsCityResponse.java
+++ src/main/java/pl/pogodane/api/AllYearsCityResponse.java
@@ -1,17 +1,28 @@
 package pl.pogodane.api;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import pl.pogodane.mongo.YearlyCityData;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class AllYearsCityResponse {
    private List<YearlyCityData> yearlyCityData;
+   private BigDecimal maximumTemperature;
+   private String maximumTemperatureYear;
+   private String maximumTemperatureMonth;
+   private BigDecimal minimumTemperature;
+   private String minimumTemperatureYear;
+   private String minimumTemperatureMonth;
+   private BigDecimal maximumSnowHeight;
+   private String maximumSnowHeightYear;
+   private String maximumSnowHeightMonth;
+   private BigDecimal maximumRainfallAmount;
+   private String maximumRainfallAmountYear;
+   private String maximumRainfallAmountMonth;
 }
--- src/main/java/pl/pogodane/service/WeatherService.java
+++ src/main/java/pl/pogodane/service/WeatherService.java
@@ -9,9 +9,11 @@ import pl.pogodane.mongo.YearlyCityData;
 import pl.pogodane.mongo.repositories.MonthlyCityDataRepostiory;
 import pl.pogodane.mongo.repositories.YearlyCityDataRepository;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @Service
 public class WeatherService {
@@ -30,6 +32,37 @@ public class WeatherService {
    public AllYearsCityResponse createAllYearCityResponse(String cityTechnicalId) {
       List<YearlyCityData> yearlyCityData = new ArrayList<>(yearlyCityDataRepository.findAllByCityTechnicalId(cityTechnicalId));
 
-      return new AllYearsCityResponse(yearlyCityData);
+      YearlyCityData maximumTemperature = yearlyCityData.stream()
+         .filter(data -> data.getMaximumTemperature() != null)
+         .max(Comparator.comparing(YearlyCityData::getMaximumTemperature))
+         .orElseGet(() -> new YearlyCityData());
+      YearlyCityData minimumTemperature = yearlyCityData.stream()
+         .filter(data -> data.getMinimumTemperature() != null)
+         .min(Comparator.comparing(YearlyCityData::getMinimumTemperature))
+         .orElseGet(() -> new YearlyCityData());
+      YearlyCityData maximumSnowHeight = yearlyCityData.stream()
+         .filter(data -> data.getMaximumSnowHeight() != null)
+         .max(Comparator.comparing(YearlyCityData::getMaximumSnowHeight))
+         .orElseGet(() -> new YearlyCityData());
+      YearlyCityData maximumRainfallAmount = yearlyCityData.stream()
+         .filter(data -> data.getMaximumRainfallAmount() != null)
+         .max(Comparator.comparing(YearlyCityData::getMaximumRainfallAmount))
+         .orElseGet(() -> new YearlyCityData());
+
+      return AllYearsCityResponse.builder()
+         .yearlyCityData(yearlyCityData)
+         .maximumTemperature(maximumTemperature.getMaximumTemperature())
+         .maximumTemperatureMonth(maximumTemperature.getMaximumTemperatureMonth())
+         .maximumTemperatureYear(maximumTemperature.getYear())
+         .minimumTemperature(minimumTemperature.getMinimumTemperature())
+         .minimumTemperatureMonth(minimumTemperature.getMinimumTemperatureMonth())
+         .minimumTemperatureYear(minimumTemperature.getYear())
+         .maximumSnowHeight(maximumSnowHeight.getMaximumSnowHeight())
+         .maximumSnowHeightMonth(maximumSnowHeight.getMaximumSnowHeightMonth())
+         .maximumSnowHeightYear(maximumSnowHeight.getYear())
+         .maximumRainfallAmount(maximumRainfallAmount.getMaximumRainfallAmount())
+         .maximumRainfallAmountMonth(maximumRainfallAmount.getMaximumRainfallMonth())
+         .maximumRainfallAmountYear(maximumRainfallAmount.getYear())
+         .build();
    }
 }
